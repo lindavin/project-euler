f7c71476c8b50f1f3e4b9753bc224143d8905a4a
Davin Lin <lindavin@grinnell.edu>
2020-03-23 09:18:58 -0400

    Finish Problem 5

---
+++ smallest-multiples.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+;;; generates a list of integers from start to 'one' less than 'end'.
+(define range
+  (lambda (start end)
+    (if (>= start end)
+        null
+        (cons start (range (+ start 1) end)))))
+
+;;; finds the lcm of n-arguments
+(define lcm
+  (lambda xs
+    (if (null? xs)
+        null
+        (if (singleton? xs)
+            (car xs)
+            (reduce lcm-a-b xs)
+            ))))
+
+;;; finds the lcm of two arguments
+(define lcm-a-b
+  (lambda (a b)
+    (/ (* a b) (gcd a b) )))
+
+;;; finds the gcd of two arguments
+(define gcd-a-b
+  (lambda (a b)
+    (if (eq? (remainder a b) 0)
+        b
+        (gcd-a-b b (remainder a b)))))
+
+;;; reduces a list of two or more elements to
+;;; one value using the two-value function 'fun'.
+(define reduce
+  (lambda (fun lst)
+    (if (or (null? lst) (singleton? lst))
+        null
+        (reduce-helper fun lst))))
+
+;;; helper procedure for the function 'reduce'
+(define reduce-helper
+  (lambda (fun lst)
+    (if (singleton? lst)
+        (car lst)
+        (reduce-helper fun (cons (fun (car lst) (cadr lst)) (cddr lst))))))
+
+;;; checks if a list is a singleton
+(define singleton?
+  (lambda (lst)
+    (if (null? lst)
+        #f
+        (null? (cdr lst)))))
+
+(apply lcm (range 1 11))
+(apply lcm (range 1 21))
+
